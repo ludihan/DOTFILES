9d02668c270e6ba3a33963b8a7b029a6ceef7262
ludihan <65617704+ludihan@users.noreply.github.com>
2025-01-24 12:55:55 -0400

    2025-01-24

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -46,20 +46,12 @@ require('lazy').setup({
                 'pylsp',
                 'ruby_lsp',
                 'rubocop',
-                'sorbet',
+                'yamlls'
             },
             handlers = {
                 function(server_name)
                     require('lspconfig')[server_name].setup({})
                 end,
-                ruby_lsp = function()
-                    require('lspconfig').ruby_lsp.setup({
-                        init_options = {
-                            formatter = 'standard',
-                            linters = { 'standard' },
-                        },
-                    })
-                end
             },
         },
     },
